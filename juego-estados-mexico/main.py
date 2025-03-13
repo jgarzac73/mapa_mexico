d0f61c16c746a34bdcbe80d0cf17155af9a42ad7
Juan Garza <130802431+jgarzac73@users.noreply.github.com>
2025-03-12 18:35:18 -0600

    Add files via upload

---
+++ juego-estados-mexico/main.py
@@ -0,0 +1,43 @@
+import turtle
+import pandas
+import random
+
+screen = turtle.Screen()
+screen.title("Juego de los estados de México")
+
+image = "mapa_mexico.gif"
+screen.addshape(image)
+screen.setup(width=800, height=600)
+turtle.shape(image)
+
+data = pandas.read_csv("32_estados.csv")
+estados = data["estado"].to_list()
+estados_encontrados = []
+lapiz = turtle.Turtle()
+lapiz.hideturtle()
+color_bandera  = ["green", "red"]
+lapiz.penup()
+
+while len(estados_encontrados) < 32:
+    respuesta = screen.textinput(f"Estados {len(estados_encontrados)}/32",
+                                 prompt="¿Qué Estado forma parte de México?").title()
+    if respuesta == "Pausa":
+        estados_faltantes = list(set(estados) - set(estados_encontrados))
+        dicc_estados = {
+            "Estados faltantes": estados_faltantes,
+        }
+        df = pandas.DataFrame(dicc_estados)
+        df.to_csv("estados_faltantes.csv")
+        break
+    if respuesta in estados and respuesta not in estados_encontrados:
+        print("¡Bien!")
+        estados_encontrados.append(respuesta)
+        cor_x = data[data["estado"] == respuesta].x.item()
+        cor_y = data[data["estado"] == respuesta].y.item()
+        lapiz.setpos(cor_x, cor_y)
+        lapiz.color(random.choice(color_bandera))
+        lapiz.write(respuesta)
+
+print("¡Adios!")
+
+
